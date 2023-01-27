ebdd8ebbc5cb7dd23fd0f78d59e0d86421448e98
Anish George <anishgeo@thoughtworks.com>
2023-01-27 11:17:36 +0530

    feat: AL-5 Add the core application

---
+++ apps/ageorgedev/app/routes/index.tsx
@@ -0,0 +1,39 @@
+import {Link} from "@remix-run/react";
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Dummy Remix App for testing things out</h1>
+      <ul>
+        <li>
+          <Link to={'/exp'}>
+            Testing Link
+          </Link>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/blog"
+            rel="noreferrer"
+          >
+            15m Quickstart Blog Tutorial
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/jokes"
+            rel="noreferrer"
+          >
+            Deep Dive Jokes App Tutorial
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
