9ab9961346bb78d9f2d732b9137f2e5acd1f59a2
Anish George <anishgeo@thoughtworks.com>
2023-01-27 16:20:01 +0530

    feat: AL-7 Created a change that should ideally trigger ageorgedev deploy

--- libs/demo-ds/src/lib/demo-ds.tsx
+++ libs/demo-ds/src/lib/demo-ds.tsx
@@ -8,6 +8,7 @@ export function DemoDs(props: DemoDsProps) {
     <div>
       <h1>Welcome to DemoDs!</h1>
       <p>This is a design system component</p>
+      <p>This component has now been updated</p>
     </div>
   );
 }
