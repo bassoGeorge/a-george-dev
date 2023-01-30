f5aed882f363f217a4f65c05919a2517d0b59a29
Anish George <anishgeo@thoughtworks.com>
2023-01-30 15:39:30 +0530

    feat: AL-7 Update env variables to work around the restrictions of netlify

--- apps/ageorgedev/app/models/dynamoDbClient.server.ts
+++ apps/ageorgedev/app/models/dynamoDbClient.server.ts
@@ -1,8 +1,8 @@
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBClient, DynamoDBClientConfig } from '@aws-sdk/client-dynamodb';
 
-const params =
+let params: DynamoDBClientConfig =
   process.env.NODE_ENV !== 'production' &&
-  process.env.USE_LOCAL_DYNAMODB == 'true'
+  process.env.USE_LOCAL_DYNAMO_DB == 'true'
     ? {
         region: 'localhost',
         endpoint: 'http://localhost:8000',
@@ -10,5 +10,15 @@ const params =
     : {
         region: 'ap-south-1',
       };
+
+if (process.env.DYNAMO_DB_ACCCESS_ID && process.env.DYNAMO_DB_SECRET) {
+  params = {
+    ...params,
+    credentials: {
+      accessKeyId: process.env.DYNAMO_DB_ACCCESS_ID,
+      secretAccessKey: process.env.DYNAMO_DB_SECRET,
+    },
+  };
+}
 
 export const ddbClient = new DynamoDBClient(params);
