9f2b26dce1a15b3ec99532ab6546e7828f4b1b56
Anish George <anishgeo@thoughtworks.com>
2023-02-18 16:09:06 +0530

    completes LB-32: Upgrade remix

--- apps/ageorgedev/remix.config.js
+++ apps/ageorgedev/remix.config.js
@@ -2,14 +2,13 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  serverBuildTarget: 'netlify',
   server:
     process.env.NETLIFY || process.env.NETLIFY_LOCAL
       ? './server.js'
       : undefined,
   ignoredRouteFiles: ['**/.*', '**/*.@(spec|test).+(ts|tsx|js|jsx)'],
+  serverBuildPath: '.netlify/functions-internal/server.js',
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
-  // serverBuildPath: ".netlify/functions-internal/server.js",
   // publicPath: "/build/",
 };
