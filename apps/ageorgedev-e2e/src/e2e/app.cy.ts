4561763a54e5d2fd55c6c4e2a86029d6f8ef8945
Anish George <anishgeo@thoughtworks.com>
2023-02-19 11:16:24 +0530

    completes LB-31: Build the under construction site

--- apps/ageorgedev-e2e/src/e2e/app.cy.ts
+++ apps/ageorgedev-e2e/src/e2e/app.cy.ts
@@ -1,15 +1,8 @@
-import { getGreeting } from '../support/app.po';
-
 describe('ageorgedev', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    console.log('blahhh');
-    // Custom command example, see `../support/commands.ts` file
-    // cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    // getGreeting().contains('Welcome ageorgedev');
-    cy.findByText('Dummy Remix App for testing things out');
+  it('should display under construction site', () => {
+    cy.findByRole('heading', { name: 'Anish George' });
+    cy.findByText('🚧 Site under construction 🚧');
   });
 });
--- apps/ageorgedev/app/routes/index.tsx
+++ apps/ageorgedev/app/routes/index.tsx
@@ -1,44 +1,5 @@
-import { Link } from '@remix-run/react';
-import { DemoDs } from '@ageorgedev/demo-ds';
+import { SiteUnderConstruction } from '../components/SiteUnderConstruction';
 
 export default function Index() {
-  return (
-    <div>
-      <h1 className={'text-xl font-serif text-bold'}>
-        Dummy Remix App for testing things out
-      </h1>
-      <p className={'text-green-600 font-bold'}>
-        If tailwind is in play, this text should be green in color
-      </p>
-      <DemoDs />
-      <ul>
-        <li>
-          <Link to={'/exp'}>Testing Link</Link>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+  return <SiteUnderConstruction />;
 }
