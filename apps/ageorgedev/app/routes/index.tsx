b5649e7eed25b5f13a6e6c6f0a15499e1f19bef8
Anish George <anishgeo@thoughtworks.com>
2023-02-20 10:17:43 +0530

    completing LB-25: Clean up the home page and move experiments to exp route

--- apps/ageorgedev/app/routes/index.tsx
+++ apps/ageorgedev/app/routes/index.tsx
@@ -1,11 +1,5 @@
 import { SiteUnderConstruction } from '../components/SiteUnderConstruction';
-import { DemoDs } from '@ageorgedev/demo-ds';
 
 export default function Index() {
-  return (
-    <>
-      <SiteUnderConstruction />
-      <DemoDs></DemoDs>
-    </>
-  );
+  return <SiteUnderConstruction />;
 }
