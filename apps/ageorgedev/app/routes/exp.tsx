f3a59106457be28d6f48a9497aef4e33a5854d32
Anish ‘basso’ George <anishgeorgehb@gmail.com>
2023-02-20 10:31:04 +0530

    LB-25 CSS bundling with VanillaExtract (#12)
    * completing LB-25: Add bundling support to remix app
    
    * completing LB-25: Add Vanilla Extract and get that working
    
    * completing LB-25: Clean up the home page and move experiments to exp route
    
    * completing LB-25: Configure tests to work with Vanilla Extract

--- apps/ageorgedev/app/routes/exp.tsx
+++ apps/ageorgedev/app/routes/exp.tsx
@@ -1,28 +1,10 @@
-import type { LoaderArgs } from '@remix-run/node';
-import { json } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
-import { getAllCharactersFromDb } from '../models/characters.server';
-
-export async function loader(args: LoaderArgs) {
-  return json({
-    data: await getAllCharactersFromDb(),
-    dummy: 'Just dummy updates v2',
-  });
-}
+import { DemoDs } from '@ageorgedev/demo-ds';
 
 export default function Exp() {
-  const { data } = useLoaderData<typeof loader>();
-
   return (
     <>
-      <h3>Experimental page with loader info</h3>
-      <ul>
-        {data.map((character) => (
-          <li key={character.name}>
-            {character.name} - {character.type}
-          </li>
-        ))}
-      </ul>
+      <h3 className="text-4xl mb-3">Running Experiments</h3>
+      <DemoDs></DemoDs>
     </>
   );
 }
