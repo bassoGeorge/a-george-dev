ceb55649097a5d4c4a7e5ff91e792b95011fdc36
Anish George <anishgeorgehb@gmail.com>
2023-02-22 11:50:22 +0530

    completing LB-40: WIP get initial skew transforms correct

--- apps/ageorgedev/app/components/SiteUnderConstruction.css.ts
+++ apps/ageorgedev/app/components/SiteUnderConstruction.css.ts
@@ -1,5 +1,81 @@
-import { style } from '@vanilla-extract/css';
+import { createVar, style } from '@vanilla-extract/css';
 
-export const testDrive = style({
-  color: 'green',
+const deg = createVar('skewDeg');
+const nDeg = createVar('negativeSkewDeg');
+
+export const container = style([
+  {
+    vars: {
+      [deg]: '3deg',
+      [nDeg]: `calc(${deg} * -1)`,
+    },
+    gridTemplateColumns: '1fr',
+    gridTemplateRows: '2fr 1fr max-content',
+  },
+  onTablet({
+    gridTemplateColumns: '4fr 3fr',
+    gridTemplateRows: '1fr max-content',
+  }),
+  onDesktop({
+    vars: {
+      [deg]: '2deg',
+    },
+    gridTemplateColumns: '5fr 3fr',
+  }),
+]);
+
+export const nameSection = style({});
+
+export const subTextSection = style(
+  onTablet({
+    zIndex: 1,
+    // background: 'black',
+    background: '#FFF1D8',
+    ':before': {
+      content: '',
+      position: 'absolute',
+      background: '#FFF1D8',
+      top: 0,
+      left: 0,
+      bottom: 0,
+      right: 0,
+      zIndex: -1,
+      transform: `skew(${nDeg}, ${deg})`,
+      transformOrigin: 'top left',
+    },
+  })
+);
+
+export const conSection = style({
+  zIndex: 2,
+  // background: 'black',
+  background: '#FFF7E8',
+  ':before': {
+    content: '',
+    position: 'absolute',
+    background: '#FFF7E8',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    zIndex: -1,
+    transform: `skew(0deg, ${deg})`,
+    transformOrigin: 'top right',
+  },
 });
+
+function onTablet(styles: object) {
+  return {
+    '@media': {
+      'screen and (min-width: 768px)': styles,
+    },
+  };
+}
+
+function onDesktop(styles: object) {
+  return {
+    '@media': {
+      'screen and (min-width: 1024px)': styles,
+    },
+  };
+}
--- apps/ageorgedev/app/components/SiteUnderConstruction.tsx
+++ apps/ageorgedev/app/components/SiteUnderConstruction.tsx
@@ -1,13 +1,32 @@
-import { testDrive } from './SiteUnderConstruction.css';
+import {
+  conSection,
+  container,
+  nameSection,
+  subTextSection,
+} from './SiteUnderConstruction.css';
 
 export function SiteUnderConstruction() {
   return (
-    <main className="grid items-center w-screen h-screen bg-[#FFF1D8]">
-      <section className="text-center text-[#0D2124]">
-        <h1 className="font-serif text-[140px]">Anish George</h1>
-        <small className={'text-[40px] ' + testDrive}>
-          🚧 Site under construction 🚧
-        </small>
+    <main
+      className={`${container} grid w-screen h-screen place-items-stretch relative overflow-hidden `}
+    >
+      <section
+        className={`${nameSection} flex flex-col justify-end md:justify-center bg-[#FFEBC5]`}
+      >
+        <h1 className="font-serif text-center">Name</h1>
+      </section>
+      <section
+        className={`${subTextSection} md:p-10 bg-[#FFEBC5] md:bg-none flex flex-col justify-start md:justify-end relative`}
+      >
+        <div className="text-center md:text-right">
+          <h3>Web Developer</h3>
+          <p>Architecting web experiences since 2016</p>
+        </div>
+      </section>
+      <section
+        className={`${conSection} p-10 md:col-span-2 flex justify-center relative`}
+      >
+        <p>🚧 Site under construction 🚧</p>
       </section>
     </main>
   );
