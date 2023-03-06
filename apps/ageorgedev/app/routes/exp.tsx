755dbd80cdec814b23455fc1badd7a4d12dc29b9
Anish George <anishgeorgehb@gmail.com>
2023-03-06 15:39:12 +0530

    completes LB-00: Prepping for next story
    We are going to keep demo-ds separate to have affected checks

--- apps/ageorgedev/app/routes/exp.tsx
+++ apps/ageorgedev/app/routes/exp.tsx
@@ -1,10 +1,7 @@
-import { DemoDs } from '@ageorgedev/demo-ds';
-
 export default function Exp() {
   return (
     <>
       <h3 className="text-4xl mb-3">Running Experiments</h3>
-      <DemoDs></DemoDs>
     </>
   );
 }
