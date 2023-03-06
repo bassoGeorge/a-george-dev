b97f795a251570aa4a2dd3910732f268214df543
Anish ‘basso’ George <anishgeorgehb@gmail.com>
2023-03-07 01:21:23 +0530

    fixes LB-00: Checking if workflow is good. (#21)

--- apps/ageorgedev/app/routes/exp.tsx
+++ apps/ageorgedev/app/routes/exp.tsx
@@ -2,6 +2,7 @@ export default function Exp() {
   return (
     <>
       <h3 className="text-4xl mb-3">Running Experiments</h3>
+      <p>Here is a change that will affect the site</p>
     </>
   );
 }
