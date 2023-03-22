93601ae203c177e69cd75bd610987234eaabfffb
Anish George <anishgeorgehb@gmail.com>
2023-03-22 23:15:28 +0530

    fixes LB-14: Add more documentation for foundation-utils

--- libs/foundation-styles/src/lib/utils.spec.ts
+++ libs/foundation-styles/src/lib/utils.spec.ts
@@ -1,7 +1,6 @@
 import {
   cssCase,
   cssVarName,
-  fullCssVarName,
   getTailwindPropertyMap,
   joinCssClassParts,
   toRem,
@@ -10,14 +9,6 @@ import { describe, expect } from 'vitest';
 
 describe('Foundation Utils', () => {
   describe('createCssVarName', () => {
-    it('creates css variable names correctly', () => {
-      expect(fullCssVarName(['something', 'good'])).toEqual(
-        '--ag-something-good'
-      );
-      expect(fullCssVarName(['pascalCase'])).toEqual('--ag-pascal-case');
-      expect(fullCssVarName(['other case'])).toEqual('--ag-other-case');
-    });
-
     it('creates base css variable names correctly', () => {
       expect(cssVarName(['something', 'good'])).toEqual('ag-something-good');
       expect(cssVarName(['pascalCase'])).toEqual('ag-pascal-case');
