3db592e2f9e4f6031aa96051099e08ea5ba3c30c
Anish George <anishgeorgehb@gmail.com>
2023-03-25 15:39:01 +0530

    fixes LB-57: Update the theme switcher to be just a button

--- apps/ageorgedev/app/components/ThemeSwitcher.tsx
+++ apps/ageorgedev/app/components/ThemeSwitcher.tsx
@@ -4,14 +4,16 @@ import { useTheme } from '@ageorgedev/molecules';
 
 export function ThemeSwitcher() {
   const { theme, setTheme } = useTheme();
+  const nextTheme = theme === 'dark' ? 'light' : 'dark';
 
   const switchTheme = useCallback(() => {
-    setTheme(theme === 'dark' ? 'light' : 'dark');
-  }, [theme, setTheme]);
+    setTheme(nextTheme);
+  }, [nextTheme, setTheme]);
 
   return (
     <button
-      className={`font-interface text-cc-neutral-300 p-3 leading-none`}
+      className={`font-interface text-2xl text-cc-neutral-300 hover:text-rc-s-accent-400 p-3 leading-none transition-colors`}
+      title={`Switch to ${nextTheme} theme`}
       onClick={switchTheme}
     >
       {theme === 'dark' ? <Sun weight="duotone" /> : <Moon weight="duotone" />}
