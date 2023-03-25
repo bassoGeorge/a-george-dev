d87c59d479a35cab8279b65f2508cb7ebc23a6cd
Anish George <anishgeorgehb@gmail.com>
2023-03-25 23:13:52 +0530

    fixes LB-49: Update e2e tests

--- apps/ageorgedev-e2e/src/e2e/app.cy.ts
+++ apps/ageorgedev-e2e/src/e2e/app.cy.ts
@@ -2,8 +2,9 @@ describe('ageorgedev', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display under construction site', () => {
-    cy.findByRole('heading', { name: 'Anish George' });
-    cy.findByText('🚧 Site under construction 🚧');
+    cy.findByRole('heading', { name: 'Anish' });
+    cy.findByRole('heading', { name: 'George' });
+    cy.findByText('Site under construction');
     cy.findByRole('heading', { name: 'Web Developer' });
   });
 });
