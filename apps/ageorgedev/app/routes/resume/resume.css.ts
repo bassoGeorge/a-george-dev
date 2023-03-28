d929195d730264c15eb9d47a43f4a4df10068429
Anish George <anishgeorgehb@gmail.com>
2023-03-28 12:29:40 +0530

    completing LB-56: Print ready base layout

--- apps/ageorgedev/app/routes/resume/resume.css.ts
+++ apps/ageorgedev/app/routes/resume/resume.css.ts
@@ -1,4 +1,25 @@
-import { style } from '@vanilla-extract/css';
+import { createVar, globalStyle, style } from '@vanilla-extract/css';
+import { ContextualColors } from '@ageorgedev/foundation-styles';
+
+globalStyle('body', {
+  background: 'red',
+});
+
+const bg = createVar();
+
+const rotation = 'rotate(-2deg)';
+const paper = style([
+  'relative before:inset-0 before:absolute',
+  {
+    ':before': {
+      background: bg,
+      zIndex: -1,
+      transform: rotation,
+    },
+  },
+]);
+
+const loremStyles = 'p-6 flex flex-col gap-3';
 
 export const page = style([
   'grid',
@@ -10,11 +31,25 @@ export const page = style([
     'header header'
     'aside article'
   `,
+    overflow: 'hidden',
+    printColorAdjust: 'exact',
   },
 ]);
 
 export const header = style([
-  'bg-cc-page-0',
+  loremStyles,
+  {
+    vars: {
+      [bg]: ContextualColors.page[0],
+    },
+    zIndex: 2,
+    background: bg,
+    ':before': {
+      transformOrigin: '33.33% center',
+      transform: `${rotation} scale(1.1, 1.5)`,
+    },
+  },
+  paper,
   {
     gridArea: 'header',
     height: 130,
@@ -22,14 +57,37 @@ export const header = style([
 ]);
 
 export const aside = style([
-  'bg-cc-page-1',
+  loremStyles,
+  'pt-10',
+  {
+    vars: {
+      [bg]: ContextualColors.page[1],
+    },
+    zIndex: 1,
+    ':before': {
+      transformOrigin: 'top right',
+      transform: `${rotation} scale(1.2, 1)`,
+    },
+  },
+  paper,
   {
     gridArea: 'aside',
   },
 ]);
 
 export const article = style([
-  'bg-cc-page-4',
+  loremStyles,
+  {
+    vars: {
+      [bg]: ContextualColors.page[4],
+    },
+    zIndex: 3,
+    ':before': {
+      transformOrigin: 'top left',
+      transform: `${rotation} scale(1, 1.5)`,
+    },
+  },
+  paper,
   {
     gridArea: 'article',
   },
--- apps/ageorgedev/app/routes/resume/route.tsx
+++ apps/ageorgedev/app/routes/resume/route.tsx
@@ -1,12 +1,77 @@
-import { Heading1, Heading4 } from '@ageorgedev/atoms';
+import { BodyMd } from '@ageorgedev/atoms';
 import { article, aside, header, page } from './resume.css';
+import { GithubLogo } from '@phosphor-icons/react';
 
 export default function Resume() {
   return (
     <main className={page}>
-      <header className={header}></header>
-      <aside className={aside}></aside>
-      <article className={article}></article>
+      <header className={header}>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut,
+          tempore!
+        </p>
+        <p>
+          Consequuntur id nihil non pariatur voluptatum. Atque iste minus
+          obcaecati!
+        </p>
+        <p className="text-rc-p-accent-300">
+          Ab commodi dicta dolores, minus nihil officia perspiciatis quam
+          soluta.
+        </p>
+      </header>
+      <aside className={aside}>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni nam
+          quas suscipit ullam!
+        </p>
+        <p>
+          Aliquam blanditiis cupiditate iste molestiae nesciunt nihil, omnis
+          quisquam sed tenetur voluptatem! Omnis.
+        </p>
+        <p>
+          Aut autem enim eveniet excepturi illo, libero quae, quidem quis, sunt
+          tempora totam!
+        </p>
+        <p>
+          At autem cum eius harum ipsum nihil reiciendis sequi sunt? Dolorum,
+          magni, velit.
+        </p>
+        <p>
+          Dignissimos facilis in neque tempora! Animi dicta explicabo fugiat,
+          laboriosam maiores ratione unde.
+        </p>
+      </aside>
+      <article className={article}>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, dolore?
+        </p>
+        <p>A ad cupiditate enim est explicabo nisi odio omnis vitae.</p>
+        <p>
+          Alias consequatur doloribus eaque, ex iure neque possimus quasi
+          veritatis?
+        </p>
+        <p className="bg-cc-page-0">
+          Dicta distinctio eum itaque maxime molestias nisi nobis nostrum,
+          voluptatem.
+        </p>
+        <p className="flex gap-3 justify-start items-center">
+          <a
+            href="https://github.com/bassoGeorge"
+            target="_blank"
+            className={BodyMd.classes}
+            rel="noreferrer"
+          >
+            <GithubLogo weight="duotone"></GithubLogo>
+          </a>
+          <a
+            href="https://github.com/bassoGeorge"
+            target="_blank"
+            rel="noreferrer"
+          >
+            https://github.com/bassoGeorge
+          </a>
+        </p>
+      </article>
     </main>
   );
 }
