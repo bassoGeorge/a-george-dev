78987c3a22a8ab5fc2359509df696e565d11290e
Anish George <anishgeorgehb@gmail.com>
2023-04-08 12:03:09 +0530

    completing LB-69: Remove the demo-ds codebase

--- libs/demo-ds/.storybook/main.js
+++
@@ -1,7 +0,0 @@
-import buildStorybookMain from '../../../.storybook/main';
-
-export default buildStorybookMain({ projectPath: 'libs/demo-ds' });
-
-// To customize your Vite configuration you can use the viteFinal field.
-// Check https://storybook.js.org/docs/react/builders/vite#configuration
-// and https://nx.dev/packages/storybook/documents/custom-builder-configs
--- libs/demo-ds/src/lib/demo-ds.css.ts
+++
@@ -1,5 +0,0 @@
-import { style } from '@vanilla-extract/css';
-
-export const greenText = style({
-  color: 'var(--ag-color-s-accent-300)',
-});
