1c5b2b4c2a04acf9ed0aeb1828bd8f96a0d944c2
Anish George <anishgeorgehb@gmail.com>
2023-04-13 23:56:03 +0530

    completing LB-70: Got the main application connected to the database

--- apps/ageorgedev/app/routes/exp.tsx
+++ apps/ageorgedev/app/routes/exp.tsx
@@ -1,8 +1,26 @@
+import type { LoaderArgs } from '@remix-run/node';
+import { json } from '@remix-run/node';
+import { getAllPosts } from '../server/posts.server';
+import { useLoaderData } from '@remix-run/react';
+
+export async function loader(args: LoaderArgs) {
+  const posts = await getAllPosts();
+  return json({ posts });
+}
+
 export default function Exp() {
+  const { posts } = useLoaderData<typeof loader>();
   return (
     <>
       <h3 className="text-4xl mb-3">Running Experiments</h3>
       <p>Here is a change that will affect the site</p>
+      {posts.map((post) => (
+        <div key={post.id} className="mt-3">
+          <h3>{post.title}</h3>
+          <p>Status: {post.status}</p>
+          <p>Author: {post.author?.name}</p>
+        </div>
+      ))}
     </>
   );
 }
