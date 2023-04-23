7b1e04dad7f6d5af29f269390de59fd3fc7075d9
Anish George <anishgeorgehb@gmail.com>
2023-04-23 23:32:52 +0530

    completing LB-71: Get a seed strategy added for cypress

---
+++ apps/ageorgedev-e2e/src/support/db-seed.ts
@@ -0,0 +1,43 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function userPostSeed() {
+  const testUser = await prisma.user.upsert({
+    where: { email: 'tu@test.com' },
+    update: {},
+    create: {
+      email: 'tu@test.com',
+      name: 'Test User',
+      posts: {
+        create: [
+          {
+            title: 'First Testing Post',
+            publishedAt: new Date('2023-01-01'),
+            status: 'published',
+          },
+          {
+            title: 'Second Testing Post',
+            publishedAt: new Date('2023-01-02'),
+            status: 'published',
+          },
+          {
+            title: 'Draft Testing Post',
+            status: 'draft',
+          },
+        ],
+      },
+    },
+  });
+}
+
+export async function seedDatabase() {
+  await userPostSeed();
+}
+
+seedDatabase()
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  })
+  .finally(() => prisma.$disconnect());
