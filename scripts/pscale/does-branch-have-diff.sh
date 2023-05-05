fbb2bb6d8f4d6343121a1d8b56be6e0ae71fbb72
Anish George <anishgeorgehb@gmail.com>
2023-05-05 20:19:45 +0530

    completing LB-72: Added script to check if we need to create a DR or not

---
+++ scripts/pscale/does-branch-have-diff.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ORG_NAME=lands-between
+DB_NAME=ageorgedev
+BRANCH_NAME=$1
+
+number_of_diffs=$(pscale branch diff "$DB_NAME" "$BRANCH_NAME" --org "$ORG_NAME" -f json | jq "length")
+
+if [ "$number_of_diffs" -eq 0 ]; then
+  echo "Branch $BRANCH_NAME has no diffs against parent, no need to create deploy request";
+  if [ -n "$CI" ]; then
+    echo "create_dr=false" >>$GITHUB_OUTPUT
+  fi
+else
+  echo "Branch $BRANCH_NAME has diffs against parent, should create deploy request";
+  if [ -n "$CI" ]; then
+    echo "create_dr=true" >>$GITHUB_OUTPUT
+  fi
+fi
