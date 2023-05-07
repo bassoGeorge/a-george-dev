c9350d8c91a7a16c6f184b4d13d7c1e8973f7e23
Anish George <anishgeorgehb@gmail.com>
2023-05-07 15:16:22 +0530

    completing LB-72: new strategy for separate connection string script

--- scripts/pscale/create-branch.sh
+++ scripts/pscale/create-branch.sh
@@ -15,30 +15,7 @@ fi
 
 . ./ps-branch-helper-functions.sh
 echo "Deleting branch $BRANCH_NAME if it already exists..."
-forceDeleteDbBranch "$ORG_NAME" "$DB_NAME" "$BRANCH_NAME"
+pscale branch delete "$DB_NAME" "$BRANCH_NAME" --force --org "$ORG_NAME" >/dev/null 2>&1
 
 echo "Creating branch $BRANCH_NAME..."
-branch_url=$(createDbBranch "$ORG_NAME" "$DB_NAME" "$BRANCH_NAME")
-
-if [ $? -ne 0 ]; then
-  echo "Failed to create branch $BRANCH_NAME"
-  exit 1
-fi
-
-echo "Successfully created branch $BRANCH_NAME"
-echo "Generating password and connection string..."
-
-connection_string=$(addBranchSecurityAndCreateConnectionString "$ORG_NAME" "$DB_NAME" "$BRANCH_NAME")
-
-if [ $? -ne 0 ]; then
-  echo "Failed to generate connection string for branch $BRANCH_NAME"
-  exit 1
-fi
-
-echo "Successfully generated connection string for branch $BRANCH_NAME"
-
-if [ -n "$CI" ]; then
-  echo "Setting environment variables for branch url and connection string..."
-  echo "DB_BRANCH_URL=$branch_url" >>$GITHUB_OUTPUT
-  echo "DB_BRANCH_CONNECTION_STRING=$connection_string" >>$GITHUB_OUTPUT
-fi
+pscale branch create "$DB_NAME" "$BRANCH_NAME" --org "$ORG_NAME" --wait
