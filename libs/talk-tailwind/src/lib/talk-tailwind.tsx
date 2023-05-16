e3e07fefd7e274d0356b2110adc5ea3064e9d1ad
Anish George <anishgeorgehb@gmail.com>
2023-05-16 12:19:12 +0530

    completes LB-88: Refactor reveal js code and pull out a framework
    1. Enable theming for reveal.js
    2. Create base library for reveal
    4. Move the reveal globals to reveal-framework lib

--- libs/talk-tailwind/src/lib/talk-tailwind.tsx
+++ libs/talk-tailwind/src/lib/talk-tailwind.tsx
@@ -1,17 +1,15 @@
-import { Body, BodyMd, Heading3 } from '@ageorgedev/atoms';
-import useRevealJs from './hooks/useRevealJs';
+import { Body, Heading1 } from '@ageorgedev/atoms';
+import { useRevealFramework } from '@ageorgedev/reveal-framework';
 
 export function TalkTailwind() {
-  useRevealJs();
+  useRevealFramework();
 
   return (
     <div>
       <div className="reveal w-full h-screen">
         <div className="slides">
           <section>
-            <Heading3 className="border border-rc-p-accent-200">
-              Welcome to The Tailwid Talk
-            </Heading3>
+            <Heading1>Welcome to The Tailwind Talk</Heading1>
             <Body>Just a testing slide for now</Body>
           </section>
           <section>Slide 2</section>
