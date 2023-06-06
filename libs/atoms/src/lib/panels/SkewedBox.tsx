a2de20899285ad9925a48309de8f36bb311360fe
Anish George <anishgeorgehb@gmail.com>
2023-06-06 14:48:06 +0530

    fixes LB-101: Use vanilla-extract/recipes for cleaner code in skewedbox

--- libs/atoms/src/lib/panels/SkewedBox.tsx
+++ libs/atoms/src/lib/panels/SkewedBox.tsx
@@ -1,50 +1,37 @@
+import { RecipeVariants } from '@vanilla-extract/recipes';
 import {
-  interactiveShadow,
-  shapes,
-  skewStrength,
-  skewedBox,
-  skewedBoxBorder,
-  skewedBoxShadow,
+  skewedBoxBorderContainer,
+  skewedBoxContents,
+  skewedBoxShadowContainer,
 } from './panels.css';
 
-type Shape = 'trapRight' | 'trapLeft' | 'triUpperRight' | 'triUpperLeft';
-type Border = 'none' | 'all' | 'bottom';
-
 type SkewedBoxProps = {
-  shape?: Shape;
-  border?: Border;
-  interactive?: boolean;
   outerClassName?: string;
-};
+} & RecipeVariants<typeof skewedBoxShadowContainer> &
+  RecipeVariants<typeof skewedBoxBorderContainer> &
+  React.HTMLProps<HTMLDivElement>;
 
 export function SkewedBox({
   children,
-  shape,
-  border,
   className,
   outerClassName,
   interactive,
-  ...otherProps
-}: React.HTMLProps<HTMLDivElement> & SkewedBoxProps) {
-  const skewShape = shape ? shapes[shape] : '';
-  border = border ?? 'all';
-
+  shape,
+  border,
+  ...htmlProps
+}: SkewedBoxProps) {
   return (
     <div
-      {...otherProps}
-      className={`${skewStrength} ${skewShape} ${skewedBoxShadow} ${
-        interactive ? interactiveShadow : ''
-      } ${outerClassName ?? ''}`}
+      {...htmlProps}
+      className={`${skewedBoxShadowContainer({ shape, interactive })} ${
+        outerClassName ?? ''
+      }`}
     >
-      <div className={`${skewedBoxBorder} ${fakeBorderClasses[border]}`}>
-        <div className={`${skewedBox} ${className ?? ''}`}>{children}</div>
+      <div className={skewedBoxBorderContainer({ border })}>
+        <div className={`${skewedBoxContents} ${className ?? ''}`}>
+          {children}
+        </div>
       </div>
     </div>
   );
 }
-
-const fakeBorderClasses: Record<Border, string> = {
-  none: '',
-  all: 'p-1',
-  bottom: 'pb-1',
-};
--- libs/atoms/src/lib/panels/panels.css.ts
+++ libs/atoms/src/lib/panels/panels.css.ts
@@ -1,10 +1,6 @@
 import { ContextualColors, Spacing } from '@ageorgedev/foundation-styles';
-import {
-  createVar,
-  fallbackVar,
-  style,
-  styleVariants,
-} from '@vanilla-extract/css';
+import { createVar, fallbackVar, style } from '@vanilla-extract/css';
+import { recipe } from '@vanilla-extract/recipes';
 
 const d = createVar('delta');
 const tlY = createVar('tlY');
@@ -17,34 +13,9 @@ const trX = createVar('trX');
 const blX = createVar('blX');
 const brX = createVar('brX');
 
-export const skewStrength = style({
+const skewStrength = style({
   vars: {
     [d]: Spacing[2],
-  },
-});
-
-export const shapes = styleVariants({
-  trapRight: {
-    vars: {
-      [trY]: d,
-      [brY]: d,
-    },
-  },
-  trapLeft: {
-    vars: {
-      [tlY]: d,
-      [blY]: d,
-    },
-  },
-  triUpperRight: {
-    vars: {
-      [brY]: d,
-    },
-  },
-  triUpperLeft: {
-    vars: {
-      [blY]: d,
-    },
   },
 });
 
@@ -64,19 +35,7 @@ const skewStyle = style({
     )`,
 });
 
-export const skewedBoxBorder = style(['bg-cc-neutral-500', skewStyle]);
-
-export const skewedBox = style([
-  'p-6',
-  skewStyle,
-  {
-    backgroundColor: ContextualColors.page[1], // kind of acts as a default. can be overridden by passing in bg-cc-*
-  },
-]);
-
-export const skewedBoxShadow = style(['drop-shadow']);
-
-export const interactiveShadow = style([
+const interactiveShadow = style([
   'hover:drop-shadow-far active:drop-shadow-near transition',
   {
     ':hover': {
@@ -85,5 +44,60 @@ export const interactiveShadow = style([
     ':active': {
       transform: 'translate(2px, 4px)',
     },
+  },
+]);
+
+export const skewedBoxShadowContainer = recipe({
+  base: [skewStrength, 'drop-shadow'],
+  variants: {
+    shape: {
+      trapRight: {
+        vars: {
+          [trY]: d,
+          [brY]: d,
+        },
+      },
+      trapLeft: {
+        vars: {
+          [tlY]: d,
+          [blY]: d,
+        },
+      },
+      triUpperRight: {
+        vars: {
+          [brY]: d,
+        },
+      },
+      triUpperLeft: {
+        vars: {
+          [blY]: d,
+        },
+      },
+    },
+    interactive: {
+      true: interactiveShadow,
+    },
+  },
+});
+
+export const skewedBoxBorderContainer = recipe({
+  base: ['bg-cc-neutral-500', skewStyle],
+  variants: {
+    border: {
+      all: 'p-1',
+      bottom: 'pb-1',
+      none: '',
+    },
+  },
+  defaultVariants: {
+    border: 'all',
+  },
+});
+
+export const skewedBoxContents = style([
+  'p-6',
+  skewStyle,
+  {
+    backgroundColor: ContextualColors.page[1], // kind of acts as a default. can be overridden by passing in bg-cc-*
   },
 ]);
