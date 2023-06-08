01f76be6199b686e33edba5aae8e6b19c977d455
Anish George <anishgeorgehb@gmail.com>
2023-06-08 22:49:46 +0530

    fixes LB-106: Move the TwLogo to the brand-components library

--- libs/atoms/src/index.ts
+++ libs/atoms/src/index.ts
@@ -3,4 +3,3 @@ export * from './lib/NameLogo/ShortNameLogo';
 export * from './lib/typography/typography-components';
 export * from './lib/panels/SkewedBox';
 export * from './lib/panels/StraightBox';
-export * from './lib/brand-logos/TwLogo';
