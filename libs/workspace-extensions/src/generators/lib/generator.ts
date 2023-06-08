9d2e417e70aea4fd95958abc6059e457ec63edf1
Anish George <anishgeorgehb@gmail.com>
2023-06-08 20:59:44 +0530

    fixes LB-92: WIP for a library generator v3

--- libs/workspace-extensions/src/generators/lib/generator.ts
+++ libs/workspace-extensions/src/generators/lib/generator.ts
@@ -1,26 +1,48 @@
-import { Tree } from '@nx/devkit';
-import { LibSchema } from './schema';
+import {
+  Tree,
+  formatFiles,
+  generateFiles,
+  readProjectConfiguration,
+  updateJson,
+} from '@nx/devkit';
 import { libraryGenerator as plainLibGenerator } from '@nx/js';
-import { libraryGenerator as reactLibGenerator } from '@nx/react';
 import { Linter } from '@nx/linter';
+import { libraryGenerator as reactLibGenerator } from '@nx/react';
+import * as path from 'path';
+import { LibSchema } from './schema';
 
 export default async function (tree: Tree, schema: LibSchema) {
   console.log('Creating library with options', schema);
 
-  const commonLibSchema = {
+  const libGeneratorSchema = {
     name: schema.name,
     linter: Linter.EsLint,
     bundler: 'vite',
     unitTestRunner: 'vitest',
     buildable: true,
+    style: 'none',
   } as const;
 
-  if (schema.react) {
-    await reactLibGenerator(tree, {
-      ...commonLibSchema,
-      style: 'none',
-    });
-  } else {
-    await plainLibGenerator(tree, commonLibSchema);
-  }
+  const generator = schema.react ? reactLibGenerator : plainLibGenerator;
+
+  const genCleanup = await generator(tree, libGeneratorSchema);
+
+  const projectConfig = readProjectConfiguration(tree, schema.name);
+
+  updateJson(tree, `${projectConfig.root}/.eslintrc.json`, (config) => {
+    config.extends.push('../../.eslintrc.ts.json');
+    config.ignorePatterns = [
+      ...config.ignorePatterns,
+      'dist/**/*',
+      'generated-src/**/*',
+      '.storybook/preview.js',
+    ];
+    return config;
+  });
+
+  generateFiles(tree, path.join(__dirname, 'files'), projectConfig.root, {});
+
+  formatFiles(tree);
+
+  return genCleanup;
 }
