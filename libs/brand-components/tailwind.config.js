b686fcc7356bac754aa804b62e85661c2c1fecca
Anish George <anishgeorgehb@gmail.com>
2023-06-08 22:12:50 +0530

    fixes LB-92: Add library for brand-components

---
+++ libs/brand-components/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import("tailwindcss").Config} */
+const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
+const { join } = require('path');
+
+const fileGlob = '**/!(*.stories|*.spec)*.{ts,tsx,js,jsx}';
+
+module.exports = {
+  presets: [require('@ageorgedev/foundation-styles/tailwind')],
+  content: [
+    join(__dirname, './src/', fileGlob),
+    ...createGlobPatternsForDependencies(__dirname, fileGlob),
+  ],
+  plugins: [],
+};
