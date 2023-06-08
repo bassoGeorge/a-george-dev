bb9a8baea7dbd7d1e47508de95e601661cfec159
Anish George <anishgeorgehb@gmail.com>
2023-06-08 20:06:00 +0530

    fixes LB-92: WIP for a library generator v2

--- libs/workspace-extensions/src/generators/lib/generator.ts
+++ libs/workspace-extensions/src/generators/lib/generator.ts
@@ -1,6 +1,26 @@
 import { Tree } from '@nx/devkit';
 import { LibSchema } from './schema';
+import { libraryGenerator as plainLibGenerator } from '@nx/js';
+import { libraryGenerator as reactLibGenerator } from '@nx/react';
+import { Linter } from '@nx/linter';
 
 export default async function (tree: Tree, schema: LibSchema) {
   console.log('Creating library with options', schema);
+
+  const commonLibSchema = {
+    name: schema.name,
+    linter: Linter.EsLint,
+    bundler: 'vite',
+    unitTestRunner: 'vitest',
+    buildable: true,
+  } as const;
+
+  if (schema.react) {
+    await reactLibGenerator(tree, {
+      ...commonLibSchema,
+      style: 'none',
+    });
+  } else {
+    await plainLibGenerator(tree, commonLibSchema);
+  }
 }
