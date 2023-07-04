b3f71bfb8daea48ab27d9a7f8e32d0d98d7bd985
Anish George <anishgeorgehb@gmail.com>
2023-07-04 23:47:41 +0530

    fixes LB-113: Finish first bit of css-in-js

--- libs/talk-tailwind/src/lib/slides/s09-css-in-js.tsx
+++ libs/talk-tailwind/src/lib/slides/s09-css-in-js.tsx
@@ -1,10 +1,17 @@
-import { Heading1, Heading2, PBody, Heading3 } from '@ageorgedev/atoms';
+import {
+  Heading1,
+  Heading2,
+  PBody,
+  Heading3,
+  PBodyMd,
+} from '@ageorgedev/atoms';
 import { CodeBlock } from '@ageorgedev/molecules';
 import {
   ImportantNote,
   SlideTypeCenter,
   SlideTypeRegular,
 } from '@ageorgedev/reveal-framework';
+import { Skull, WarningDiamond } from '@phosphor-icons/react';
 
 export function S09CssInJs() {
   return (
@@ -14,26 +21,31 @@ export function S09CssInJs() {
           strategy #6
         </Heading3>
         <Heading1 as="h2" className="font-bold">
-          CSS-in-JS shenanigans
+          Tailwind CSS + CSS-in-JS
         </Heading1>
+        <Heading3 className="italic text-cc-danger">
+          <Skull className="inline-block -mt-2" /> controversial, experimental{' '}
+          <WarningDiamond className="inline-block -mt-2" />
+        </Heading3>
       </SlideTypeCenter>
-      <SlideTypeRegular heading={<Heading2>Why?...</Heading2>}>
-        <PBody className="fragment">
+      <SlideTypeCenter>
+        <Heading2>Why?...</Heading2>
+        <PBodyMd className="fragment">
           Let application logic manipulate CSS safely using design system tokens
-        </PBody>
-        <PBody className="fragment">
+        </PBodyMd>
+        <PBodyMd className="fragment">
           Migrating to tailwind from an existing system
-        </PBody>
-        <PBody className="fragment">
+        </PBodyMd>
+        <PBodyMd className="fragment">
           Some things are better done without Tailwind?
-        </PBody>
-        <PBody className="fragment">You want the flexibility</PBody>
-      </SlideTypeRegular>
+        </PBodyMd>
+        <PBodyMd className="fragment">You want the flexibility</PBodyMd>
+      </SlideTypeCenter>
       <SlideTypeRegular
         heading={<Heading2>Tailwind approved support</Heading2>}
       >
         <CodeBlock lang="javascript" text={tailwindApprovedSupport} />
-        <ImportantNote>
+        <ImportantNote type="danger" shape="triUpperRight">
           <PBody>
             Using this includes a lot of tailwind dependencies into the client
             bundle
@@ -41,6 +53,12 @@ export function S09CssInJs() {
           <PBody>There are ways around this, but be careful</PBody>
         </ImportantNote>
       </SlideTypeRegular>
+      <SlideTypeCenter>
+        <Heading2>
+          What if we <b>don't</b> want Tailwind to <br />
+          own our Design Tokens?
+        </Heading2>
+      </SlideTypeCenter>
     </section>
   );
 }
