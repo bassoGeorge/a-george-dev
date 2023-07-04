01ce42d03ac116001765703eb790832158ac9789
Anish George <anishgeorgehb@gmail.com>
2023-07-05 00:53:31 +0530

    fixes LB-113: Complete basic css-in-js stuff

--- libs/talk-tailwind/src/lib/slides/s09-css-in-js.tsx
+++ libs/talk-tailwind/src/lib/slides/s09-css-in-js.tsx
@@ -8,10 +8,12 @@ import {
 import { CodeBlock } from '@ageorgedev/molecules';
 import {
   ImportantNote,
+  SlideMediaRow,
   SlideTypeCenter,
   SlideTypeRegular,
 } from '@ageorgedev/reveal-framework';
 import { Skull, WarningDiamond } from '@phosphor-icons/react';
+import { D02CssPipeline } from '../diagrams/D02CssPipeline';
 
 export function S09CssInJs() {
   return (
@@ -59,6 +61,45 @@ export function S09CssInJs() {
           own our Design Tokens?
         </Heading2>
       </SlideTypeCenter>
+      <SlideTypeRegular heading={<Heading2>A basic token pipeline</Heading2>}>
+        <SlideMediaRow>
+          <D02CssPipeline />
+
+          <ImportantNote shape="trapLeft" className="self-end -ml-12 mb-4 w-14">
+            <PBody>
+              In this setup, the tokens file needs to be <b>commonJS</b>
+            </PBody>
+          </ImportantNote>
+        </SlideMediaRow>
+      </SlideTypeRegular>
+      <SlideTypeRegular heading={<Heading2>and here is an example</Heading2>}>
+        <div className="flex gap-7 items-start">
+          <CodeBlock lang="javascript" text={tokensFile} />
+          <CodeBlock lang="javascript" text={tokenImport} />
+        </div>
+      </SlideTypeRegular>
+
+      <SlideTypeRegular
+        heading={
+          <Heading2>
+            If you{' '}
+            <em>
+              <b>must</b>
+            </em>{' '}
+            build the tailwind config...
+          </Heading2>
+        }
+      >
+        <div className="flex items-start gap-7">
+          <CodeBlock lang="javascript" text={esBuildTailwind} />
+          <CodeBlock lang="javascript" text={esUsage} />
+        </div>
+        <ImportantNote shape="triUpperRight">
+          <PBody>
+            Make sure your preset is built before the tailwind config is used
+          </PBody>
+        </ImportantNote>
+      </SlideTypeRegular>
     </section>
   );
 }
@@ -75,3 +116,58 @@ fullConfig.theme.width[4]
 
 fullConfig.theme.screens.md
 // => '768px'`;
+
+const tokensFile = `/* tokens.js */
+
+module.exports = {
+  colors: {
+    primaryBase: 'hsl(321 12% 29%)',
+    primaryDark: 'hsl(119 22% 44%)',
+  },
+  spacingScale: [8, 12, 16, 32]
+}
+`;
+
+const tokenImport = `/* tailwind.config.js */
+const tokens = require('./tokens.js');
+export default {
+  theme: {
+    colors: {
+      primary: {
+        DEFAULT: tokens.colors.primaryBase,
+        light: tokens.colors.primaryLight,
+        dark: tokens.colors.primaryDark,
+      }
+    },
+    spacing: {
+      1: tokens.spacingScale[0] + 'px',
+      2: tokens.spacingScale[1] + 'px',
+      3: tokens.spacingScale[2] + 'px',
+      large: tokens.spacingScale[3] + 'px',
+      massive: tokens.spacingScale[4] + 'px',
+    }
+  }
+};`;
+
+const esBuildTailwind = `/* tailwind-preset.js */
+import tokens from './tokens';
+import utils from './utils';
+
+// Some fancy code
+
+export default {
+  theme: {
+    // final theme stuff
+  }
+}`;
+
+const esUsage = `/* tailwind.config.js */
+const TailwindPreset = require('./dist/core-lib/tailwind-preset.min');
+
+module.exports = {
+  presets: [TailwindPreset],
+  theme: {
+    extend: {},
+  },
+  // ...
+};`;
