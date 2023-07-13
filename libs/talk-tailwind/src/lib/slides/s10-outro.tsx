94e8cb4fed48a03d52612a41a0cf8d7cd0f8887c
Anish George <anishgeorgehb@gmail.com>
2023-07-13 23:18:40 +0530

    fixes LB-123: Add final thoughts

--- libs/talk-tailwind/src/lib/slides/s10-outro.tsx
+++ libs/talk-tailwind/src/lib/slides/s10-outro.tsx
@@ -1,9 +1,39 @@
-import { Heading1, Heading3 } from '@ageorgedev/atoms';
-import { SlideTypeCenter } from '@ageorgedev/reveal-framework';
+import {
+  Heading1,
+  Heading2,
+  Heading3,
+  PBody,
+  PBodyMd,
+} from '@ageorgedev/atoms';
+import {
+  PointSeperator,
+  SlideTypeCenter,
+  SlideTypeRegular,
+} from '@ageorgedev/reveal-framework';
 
 export function S10Outro() {
   return (
     <section>
+      <SlideTypeRegular
+        callout="Opinion"
+        heading={<Heading2>Final thoughts</Heading2>}
+      >
+        <Heading3>I am a CSS expert, I don't need Tailwind!</Heading3>
+        <PointSeperator />
+        <PBody className="fragment">Will your docs be as comprehensive?</PBody>
+        <PointSeperator />
+        <PBody className="fragment">
+          Will build tools automatically exclude unused styles?
+        </PBody>
+        <PointSeperator />
+        <PBody className="fragment">
+          Will your dev team already have experience with your CSS approach?
+        </PBody>
+        <PointSeperator />
+        <PBodyMd className="fragment font-bold text-cc-accent">
+          In fact, most of the time, we build an inferior Tailwind ourselves...
+        </PBodyMd>
+      </SlideTypeRegular>
       <SlideTypeCenter>
         <Heading3 as="h1" className="italic">
           that's all folks!
