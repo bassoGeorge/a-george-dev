81e7e0fa6c573cc598b71225809f8234c00691f4
Anish George <anish.george@fabricgroup.com.au>
2024-05-28 23:24:21 +1000

    LB-130: Add nextjs app

---
+++ apps/ageorgedev/tailwind.config.js
@@ -0,0 +1,14 @@
+const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
+const { join } = require('path');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  presets: [require('@ageorgedev/foundation-styles/tailwind')],
+  content: [
+    join(
+      __dirname,
+      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+    ),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+};
