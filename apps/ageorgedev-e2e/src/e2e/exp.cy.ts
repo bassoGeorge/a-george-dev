db3922dd443a79905a9ceda610aae683abc57aa2
Anish George <anish.george@fabricgroup.com.au>
2024-05-28 22:11:15 +1000

    LB-141: Remove DB stuff from e2e

--- apps/ageorgedev-e2e/src/e2e/exp.cy.ts
+++
@@ -1,9 +0,0 @@
-describe('Blog page', () => {
-  beforeEach(() => cy.visit('/exp'));
-
-  it('should show the blog titles', () => {
-    cy.findByRole('heading', { name: 'First Testing Post' });
-    cy.findByRole('heading', { name: 'Second Testing Post' });
-    cy.findByRole('heading', { name: 'Draft Testing Post' });
-  });
-});
