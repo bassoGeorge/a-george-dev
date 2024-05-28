a1500395fb5fab4e3c16ee08d6d60536c0f8fd37
Anish George <anish.george@fabricgroup.com.au>
2024-05-28 22:17:23 +1000

    LB-141: Remove the cms app

--- apps/cms/src/utils.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Converts a prisma field definition to a nullable field definition.
- * @param field a prisma field definition. Needs to be of the form "<name> <type> <other-attributes>"
- * @returns "<name> <type>?"
- */
-export const convertToNullableField = (field: string) => {
-  const [name, type] = field.split(' ');
-  return `${name} ${type}?`;
-};
