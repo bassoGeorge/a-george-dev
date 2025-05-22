1ea7715ea3e5a385a879f74e8d5db517b6c438f2
Anish George <anishgeorgehb@gmail.com>
2025-05-22 22:29:53 +1000

    LB-176: yarn upgrade and fix up vite configs

--- libs/molecules/vite.config.ts
+++ libs/molecules/vite.config.ts
@@ -1,34 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import viteTsConfigPaths from 'vite-tsconfig-paths';
 import dts from 'vite-plugin-dts';
-import { join } from 'path';
+import * as path from 'path';
 import { removeFilesPlugin } from './vite-plugins';
 
 export default defineConfig({
-  cacheDir: '../../node_modules/.vite/molecules',
+  root: __dirname,
 
   plugins: [
     dts({
       entryRoot: 'src',
-      tsConfigFilePath: join(__dirname, 'tsconfig.lib.json'),
-      skipDiagnostics: true,
+      tsconfigPath: path.join(__dirname, 'tsconfig.lib.json'),
     }),
     react(),
-    viteTsConfigPaths({
-      root: '../../',
-    }),
     removeFilesPlugin(),
   ],
-
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [
-  //    viteTsConfigPaths({
-  //      root: '../../',
-  //    }),
-  //  ],
-  // },
 
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
