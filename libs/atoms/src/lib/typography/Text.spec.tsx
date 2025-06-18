68b3c57d307cd423b78464dff8bdcb756675fb15
Anish George <anish.george@fabricgroup.com.au>
2025-06-18 13:58:19 +1000

    LB-000: Cleanup some tests

--- libs/atoms/src/lib/typography/Text.spec.tsx
+++ libs/atoms/src/lib/typography/Text.spec.tsx
@@ -6,9 +6,10 @@ import { TYPOGRAPHY_CLASSES } from './core-type-classes';
 describe('Text', () => {
   it('renders with default tag based on variant', () => {
     render(<Text variant="h1">Heading 1</Text>);
-    expect(screen.getByText('Heading 1')).toBeInTheDocument();
-    expect(screen.getByText('Heading 1').tagName).toBe('H1');
-    expect(screen.getByText('Heading 1')).toHaveClass(TYPOGRAPHY_CLASSES.h1);
+    const el = screen.getByText('Heading 1');
+    expect(el).toBeInTheDocument();
+    expect(el.tagName).toBe('H1');
+    expect(el).toHaveClass(TYPOGRAPHY_CLASSES.h1);
   });
 
   it('renders with custom tag', () => {
@@ -17,27 +18,26 @@ describe('Text', () => {
         Heading 1 as span
       </Text>
     );
-    expect(screen.getByText('Heading 1 as span')).toBeInTheDocument();
-    expect(screen.getByText('Heading 1 as span').tagName).toBe('SPAN');
-    expect(screen.getByText('Heading 1 as span')).toHaveClass(
-      TYPOGRAPHY_CLASSES.h1
-    );
+    const el = screen.getByText('Heading 1 as span');
+    expect(el).toBeInTheDocument();
+    expect(el.tagName).toBe('SPAN');
+    expect(el).toHaveClass(TYPOGRAPHY_CLASSES.h1);
   });
 
   it('renders body variant with paragraph tag by default', () => {
     render(<Text variant="body">Body text</Text>);
-    expect(screen.getByText('Body text')).toBeInTheDocument();
-    expect(screen.getByText('Body text').tagName).toBe('P');
-    expect(screen.getByText('Body text')).toHaveClass(TYPOGRAPHY_CLASSES.body);
+    const el = screen.getByText('Body text');
+    expect(el).toBeInTheDocument();
+    expect(el.tagName).toBe('P');
+    expect(el).toHaveClass(TYPOGRAPHY_CLASSES.body);
   });
 
   it('renders interface variant with span tag by default', () => {
     render(<Text variant="interface">Interface text</Text>);
-    expect(screen.getByText('Interface text')).toBeInTheDocument();
-    expect(screen.getByText('Interface text').tagName).toBe('SPAN');
-    expect(screen.getByText('Interface text')).toHaveClass(
-      TYPOGRAPHY_CLASSES.interface
-    );
+    const el = screen.getByText('Interface text');
+    expect(el).toBeInTheDocument();
+    expect(el.tagName).toBe('SPAN');
+    expect(el).toHaveClass(TYPOGRAPHY_CLASSES.interface);
   });
 
   it('applies custom className', () => {
@@ -46,7 +46,8 @@ describe('Text', () => {
         Heading 1
       </Text>
     );
-    expect(screen.getByText('Heading 1')).toHaveClass('custom-class');
+    const el = screen.getByText('Heading 1');
+    expect(el).toHaveClass('custom-class');
   });
 });
 
@@ -54,19 +55,19 @@ describe('TextBuilder', () => {
   it('creates a component with the specified variant', () => {
     const H1Text = TextBuilder('h1');
     render(<H1Text>Heading 1</H1Text>);
-    expect(screen.getByText('Heading 1')).toBeInTheDocument();
-    expect(screen.getByText('Heading 1').tagName).toBe('H1');
-    expect(screen.getByText('Heading 1')).toHaveClass(TYPOGRAPHY_CLASSES.h1);
+    const el = screen.getByText('Heading 1');
+    expect(el).toBeInTheDocument();
+    expect(el.tagName).toBe('H1');
+    expect(el).toHaveClass(TYPOGRAPHY_CLASSES.h1);
   });
 
   it('allows overriding the tag', () => {
     const H1Text = TextBuilder('h1');
     render(<H1Text as="span">Heading 1 as span</H1Text>);
-    expect(screen.getByText('Heading 1 as span')).toBeInTheDocument();
-    expect(screen.getByText('Heading 1 as span').tagName).toBe('SPAN');
-    expect(screen.getByText('Heading 1 as span')).toHaveClass(
-      TYPOGRAPHY_CLASSES.h1
-    );
+    const el = screen.getByText('Heading 1 as span');
+    expect(el).toBeInTheDocument();
+    expect(el.tagName).toBe('SPAN');
+    expect(el).toHaveClass(TYPOGRAPHY_CLASSES.h1);
   });
 
   it('exposes the typography classes', () => {
