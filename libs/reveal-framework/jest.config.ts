e334d87bc7eebd40462e5f5b4bbb60e1dacc8f3b
Anish George <anishgeorgehb@gmail.com>
2025-06-21 10:36:46 +1000

    LB-178: Fix all lint issues

--- libs/reveal-framework/jest.config.ts
+++ libs/reveal-framework/jest.config.ts
@@ -1,4 +1,3 @@
-/* eslint-disable */
 export default {
   displayName: 'reveal-framework',
   preset: '../../jest.preset.js',
--- libs/workspace-extensions/jest.config.ts
+++ libs/workspace-extensions/jest.config.ts
@@ -1,4 +1,3 @@
-/* eslint-disable */
 export default {
   displayName: 'workspace-extensions',
   preset: '../../jest.preset.js',
