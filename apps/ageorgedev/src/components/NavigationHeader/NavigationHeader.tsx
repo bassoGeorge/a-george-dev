f276392f4682b5f15092c5381178d795638cfe70
Anish George <anishgeorgehb@gmail.com>
2025-12-01 18:56:40 +1100

    gh-97: Add nav links

--- apps/ageorgedev/src/components/NavigationHeader/NavigationHeader.tsx
+++ apps/ageorgedev/src/components/NavigationHeader/NavigationHeader.tsx
@@ -1,13 +1,22 @@
 import {
+  InterfaceXl,
   ShortNameLogo,
   ThemeSwitcher,
   TiltCard,
 } from '@ageorgedev/design-system';
 import Link from 'next/link';
+import { cn } from '@ageorgedev/toolbelt';
 
 export type NavigationHeaderProps = {
   noLinks?: boolean;
 };
+
+const links = [
+  {
+    href: '/talks',
+    label: 'Talks',
+  },
+];
 
 export default function NavigationHeader(props: NavigationHeaderProps) {
   const HomeLink = props.noLinks ? 'span' : Link;
@@ -18,12 +27,26 @@ export default function NavigationHeader(props: NavigationHeaderProps) {
       outerClassName="relative z-50"
       className="bg-page-1 px-3 py-4"
     >
-      <header className="flex items-center justify-between">
+      <header className="flex items-center justify-between gap-4">
         <div>
           <HomeLink href="/">
             <ShortNameLogo />
           </HomeLink>
         </div>
+        <nav className="flex grow justify-end gap-2">
+          {links.map((link) => (
+            <Link
+              key={link.href}
+              href={link.href}
+              className={cn(
+                InterfaceXl.classes,
+                'focus:text-primary-foreground-0 transition-colors hover:text-primary-foreground'
+              )}
+            >
+              {link.label}
+            </Link>
+          ))}
+        </nav>
         <div>
           <ThemeSwitcher />
         </div>
