5e51e3c1ff75f178eeb6f9e772c17749da82efd8
Anish George <anishgeorgehb@gmail.com>
2026-03-22 15:28:19 +1100

    gh-109: Fix original issue with reveal

--- apps/ageorgedev/src/routes/_public/talks.tailwind.tsx
+++ apps/ageorgedev/src/routes/_public/talks.tailwind.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { TalkTailwind } from '@ageorgedev/talk-tailwind'
+import '@ageorgedev/reveal-framework/globals';
 
 export const Route = createFileRoute('/_public/talks/tailwind')({
   component: RouteComponent,
