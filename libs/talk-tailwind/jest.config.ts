7fa90d9eae1db8a032026742e941bbea09809b52
Anish George <anish.george@fabricgroup.com.au>
2026-06-04 16:14:18 +1000

    chore: working through test fixes

--- libs/talk-tailwind/jest.config.ts
+++ libs/talk-tailwind/jest.config.ts
@@ -2,22 +2,7 @@ export default {
   displayName: 'talk-tailwind',
   preset: '../../jest.preset.js',
   transform: {
-    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
-    '^.+\\.[tj]sx?$': [
-      'babel-jest',
-      {
-        presets: [
-          [
-            'next/babel',
-            {
-              'preset-react': {
-                runtime: 'automatic', // Enable modern JSX transform
-              },
-            },
-          ],
-        ],
-      },
-    ],
+    '^.+\\.[tj]sx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/libs/talk-tailwind',
