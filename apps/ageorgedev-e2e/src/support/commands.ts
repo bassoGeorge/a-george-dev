1d670758ba4ab01aae25207cdc49e2288f066ba6
Anish George <anish.george@fabricgroup.com.au>
2026-06-06 17:18:29 +1000

    chore: goodbye eslint

--- apps/ageorgedev-e2e/src/support/commands.ts
+++ apps/ageorgedev-e2e/src/support/commands.ts
@@ -9,9 +9,7 @@
 // ***********************************************
 import '@testing-library/cypress/add-commands'
 
-// // eslint-disable-next-line @typescript-eslint/no-namespace
 // declare namespace Cypress {
-//   // eslint-disable-next-line @typescript-eslint/no-unused-vars
 //   interface Chainable<Subject> {
 //     login(email: string, password: string): void;
 //   }
--- apps/ageorgedev/index.d.ts
+++ apps/ageorgedev/index.d.ts
@@ -1,6 +1,6 @@
+// biome-ignore-all lint/suspicious/noExplicitAny: Easiest way to handle svg
 import 'react'
 
-/* eslint-disable @typescript-eslint/no-explicit-any */
 declare module '*.svg' {
   const content: any
   export const ReactComponent: any
