2cc85f5ebf96a8ca3314d2ad4d476b522355cdf4
Anish George <anish.george@fabricgroup.com.au>
2026-06-10 10:43:43 +1000

    chore: Get tests working for ds

--- packages/design-system/src/test-setup.ts
+++ packages/design-system/src/test-setup.ts
@@ -1,4 +1,7 @@
+// import '@testing-library/jest-dom/vitest';
 import * as matchers from '@testing-library/jest-dom/matchers'
-import { expect } from 'vitest'
+import { cleanup } from '@testing-library/react'
+import { beforeEach, expect } from 'vitest'
 
 expect.extend(matchers)
+beforeEach(cleanup)
