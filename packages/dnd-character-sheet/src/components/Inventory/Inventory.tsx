5b11eed72818554ec92f524072f9db0a02675b66
Anish George <anishgeorgehb@gmail.com>
2026-06-21 23:35:14 +1000

    feat: Add magic item attunement

--- packages/dnd-character-sheet/src/components/Inventory/Inventory.tsx
+++ packages/dnd-character-sheet/src/components/Inventory/Inventory.tsx
@@ -1,5 +1,6 @@
 import { useCharacter } from '../CharacterSheet';
 import { HandWrittenNotes } from '../HandwrittenNotes/HandwrittenNotes';
+import { DiamondCheck } from '../layout/checkables';
 import { HorizontalDivider } from '../layout/dividers';
 import { Panel, type PanelProps } from '../layout/Panel';
 import { PanelTitle } from '../layout/PanelTitle';
@@ -15,7 +16,21 @@ export function Inventory(props: PanelProps) {
       ))}
       <HandWrittenNotes lineCount={10} className="flex-1" />
       <div>
-        <h4>Magic Item Attunement</h4>
+        <h4 className="text-xs font-bold font-interface text-neutral-subdued">
+          Magic Item Attunement
+        </h4>
+
+        <div className="flex flex-col gap-2">
+          {Array.from({ length: 3 }, (_, i) => (
+            // biome-ignore lint/suspicious/noArrayIndexKey: Not important
+            <div key={i} className="flex items-end gap-2">
+              <DiamondCheck />
+              <div className="border-b border-dotted border-neutral-disabled flex-1">
+                &nbsp;
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </Panel>
   );
