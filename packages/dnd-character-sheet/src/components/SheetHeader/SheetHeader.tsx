f9e4d6fbedfa90af6e4902414b2a051e8c3a7bfe
Anish George <anishgeorgehb@gmail.com>
2026-06-27 20:07:49 +1000

    chore: more adjustments

--- packages/dnd-character-sheet/src/components/SheetHeader/SheetHeader.tsx
+++ packages/dnd-character-sheet/src/components/SheetHeader/SheetHeader.tsx
@@ -43,7 +43,7 @@ function LevelBlock() {
   const { character } = useCharacter();
 
   return (
-    <div className="align-self-stretch flex flex-col gap-1 border-[3px] border-neutral-subdued bg-white items-center py-3 px-6 rounded-full my-2 bg-page-4">
+    <div className="align-self-stretch flex flex-col gap-1 border-[3px] border-neutral-subdued bg-white items-center py-3 px-6 rounded-full my-2 bg-page-4 z-10 -ml-6">
       <BigNumber>
         {character.classes.reduce((total, cls) => total + cls.level, 0)}
       </BigNumber>
