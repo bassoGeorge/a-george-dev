466dea27f90c02ff6a422f824fb9f58ebff31ae7
Anish George <anishgeorgehb@gmail.com>
2026-06-30 00:08:25 +1000

    feat: Add exhaustion levels

--- packages/dnd-character-sheet/src/components/SheetHeader/HealthAndDeathBlock.tsx
+++ packages/dnd-character-sheet/src/components/SheetHeader/HealthAndDeathBlock.tsx
@@ -4,47 +4,53 @@ import { VerticalDivider } from '../layout/dividers';
 import { BasicLabel, LabelUnder } from '../layout/labels';
 import { Panel } from '../layout/Panel';
 import { PanelTitle } from '../layout/PanelTitle';
-import { VerticalSubPanel } from '../layout/SubPanel';
+import { HorizontalSubPanel, VerticalSubPanel } from '../layout/SubPanel';
 
 export function HealthAndDeathBlock() {
   const { character, derived } = useCharacter();
   return (
-    <Panel className="flex p-0" topRightCorner="scooped">
-      <VerticalSubPanel className="py-2 px-3 grid grid-rows-[max-content_1fr] grid-cols-[1fr_max-content_1fr] gap-2 items-end">
-        <PanelTitle className="col-span-3">Hit Points</PanelTitle>
-        <LabelUnder>Current</LabelUnder>
-        <VerticalDivider />
-        <div>
-          <LabelUnder className="mb-2">Temp</LabelUnder>
-          <span className="text-md">{character.hitPoints.maximum}</span>
-          <LabelUnder>Max</LabelUnder>
-        </div>
-      </VerticalSubPanel>
-      <VerticalSubPanel className="py-2 px-3 grid grid-rows-[max-content_1fr] grid-cols-1 gap-2 items-end">
-        <PanelTitle>Hit Dice</PanelTitle>
-        <div>
-          <LabelUnder className="mb-2">Spent</LabelUnder>
-          <span className="text-md">
-            {derived.hitDice.map((d) => `${d.count}${d.dice}`).join(' + ')}
-          </span>
-          <LabelUnder>Max</LabelUnder>
-        </div>
-      </VerticalSubPanel>
-      <VerticalSubPanel className="py-2 px-3 flex flex-col justify-start w-25">
-        <PanelTitle className="text-left">
-          Death
-          <br />
-          Saves
-        </PanelTitle>
-        <div>
-          <EmptyCheckList count={3} kind="diamond" className="mb-1" />
-          <BasicLabel>Successes</BasicLabel>
-        </div>
-        <div>
-          <EmptyCheckList count={3} kind="diamond" className="mb-1" />
-          <BasicLabel>Failures</BasicLabel>
-        </div>
-      </VerticalSubPanel>
+    <Panel className="p-0" topRightCorner="scooped">
+      <HorizontalSubPanel className="flex items-stretch">
+        <VerticalSubPanel className="py-2 px-3 grid grid-rows-[max-content_1fr] grid-cols-[1fr_max-content_1fr] gap-2 items-end h-auto">
+          <PanelTitle className="col-span-3">Hit Points</PanelTitle>
+          <LabelUnder>Current</LabelUnder>
+          <VerticalDivider />
+          <div>
+            <LabelUnder className="mb-1">Temp</LabelUnder>
+            <span className="text-md">{character.hitPoints.maximum}</span>
+            <LabelUnder>Max</LabelUnder>
+          </div>
+        </VerticalSubPanel>
+        <VerticalSubPanel className="py-2 px-3 grid grid-rows-[max-content_1fr] grid-cols-1 gap-2 items-end h-auto">
+          <PanelTitle>Hit Dice</PanelTitle>
+          <div>
+            <LabelUnder className="mb-1">Spent</LabelUnder>
+            <span className="text-md">
+              {derived.hitDice.map((d) => `${d.count}${d.dice}`).join(' + ')}
+            </span>
+            <LabelUnder>Max</LabelUnder>
+          </div>
+        </VerticalSubPanel>
+        <VerticalSubPanel className="py-2 px-3 flex flex-col justify-start w-25 h-auto">
+          <PanelTitle className="text-left">
+            Death
+            <br />
+            Saves
+          </PanelTitle>
+          <div>
+            <EmptyCheckList count={3} kind="diamond" />
+            <BasicLabel className="-mt-1">Successes</BasicLabel>
+          </div>
+          <div className="mt-1">
+            <EmptyCheckList count={3} kind="diamond" />
+            <BasicLabel className="-mt-1">Failures</BasicLabel>
+          </div>
+        </VerticalSubPanel>
+      </HorizontalSubPanel>
+      <HorizontalSubPanel className="px-3 py-2 flex gap-2 items-center">
+        <BasicLabel>Exhaustion</BasicLabel>
+        <EmptyCheckList count={6} kind="diamond" />
+      </HorizontalSubPanel>
     </Panel>
   );
 }
