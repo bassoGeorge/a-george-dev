e7c87558fc67a4a828f23f01bd695e717aa935aa
Anish George <anishgeorgehb@gmail.com>
2026-07-05 16:46:53 +1000

    feat: get things sorted for the time being

--- apps/game-tools/src/routes/_public/dnd/characters/$slug.{-$level}.tsx
+++ apps/game-tools/src/routes/_public/dnd/characters/$slug.{-$level}.tsx
@@ -4,16 +4,21 @@ import { getCharacterBySlugAndLevel } from '../../../../data/dnd-characters';
 
 export const Route = createFileRoute('/_public/dnd/characters/$slug/{-$level}')(
   {
-    beforeLoad: () => {
-      return {
-        title: 'My character name',
-      };
-    },
     params: {
       parse: ({ slug, level }) => ({
         slug,
         level: level ? parseInt(level, 10) : undefined,
       }),
+    },
+
+    // I know we are calling the same data fetching stuff twice, but context needs
+    // to be serializable so cant put Character type in there
+    beforeLoad: ({ params }) => {
+      const pack = getCharacterBySlugAndLevel(params.slug, params.level);
+      return {
+        title: `${pack.brief.name} (level ${pack.brief.level})`,
+        spellBook: pack.spellBook,
+      };
     },
     loader: ({ params }) => {
       return getCharacterBySlugAndLevel(params.slug, params.level);
