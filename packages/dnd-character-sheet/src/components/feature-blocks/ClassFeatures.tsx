5c26329071e179867fd480d70cc3a13d6f297e0c
Anish George <anishgeorgehb@gmail.com>
2026-07-06 20:38:19 +1000

    fix(dnd): auto columns does not work with flex

--- packages/dnd-character-sheet/src/components/feature-blocks/ClassFeatures.tsx
+++ packages/dnd-character-sheet/src/components/feature-blocks/ClassFeatures.tsx
@@ -3,7 +3,6 @@ import { useCharacter } from '../CharacterSheet';
 import { FeatureList } from '../feature-blocks/FeatureList';
 import { Panel } from '../layout/Panel';
 import { PanelTitle } from '../layout/PanelTitle';
-import styles from './ClassFeatures.module.css';
 
 export function ClassFeatures() {
   const { character } = useCharacter();
@@ -12,7 +11,7 @@ export function ClassFeatures() {
     <Panel outerClasses="flex-1">
       <PanelTitle withDivider>Class Features</PanelTitle>
       <FeatureList
-        className={cn('columns-2 gap-3', styles.ColumnLayout)}
+        className={cn('columns-2 gap-3')}
         features={character.features}
       />
     </Panel>
